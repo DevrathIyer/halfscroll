063106acdace62be2cd9d4a6d3ef99511c2fba3e
Devrath Iyer <idevspms@gmail.com>
2017-11-11 23:48:22 +0000

    Commit

--- game/models.py
+++ game/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    checkpoint = models.FileField(upload_to = 'checkpoints')
+    data = models.FileField(upload_to = 'checkpoints/data')
+    index = models.FileField(upload_to = 'checkpoints/index')
