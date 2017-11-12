66af4315cb00158f851a948f3b843a84f1c33f3e
Devrath Iyer <idevspms@gmail.com>
2017-11-12 01:45:34 +0000

    Commit

--- game/static/game/build/js/resources.js
+++ game/static/game/build/js/resources.js
@@ -7,47 +7,47 @@ game.resources = [
     {
         "name": "cling",
         "type": "audio",
-        "src": "data/sfx/"
+        "src": "/static/game/data/sfx/"
     },
     {
         "name": "jump",
         "type": "audio",
-        "src": "data/sfx/"
+        "src": "/static/game/data/sfx/"
     },
     {
         "name": "stomp",
         "type": "audio",
-        "src": "data/sfx/"
+        "src": "/static/game/data/sfx/"
     },
     {
         "name": "area01_bkg0",
         "type": "image",
-        "src": "static/game/data/img/area01_bkg0.png"
+        "src": "/static/game/data/img/area01_bkg0.png"
     },
     {
         "name": "area01_bkg1",
         "type": "image",
-        "src": "static/game/data/img/area01_bkg1.png"
+        "src": "/static/game/data/img/area01_bkg1.png"
     },
     {
         "name": "title_screen",
         "type": "image",
-        "src": "static/game/data/img/gui/title_screen.png"
+        "src": "/static/game/data/img/gui/title_screen.png"
     },
     {
         "name": "area01_level_tiles",
         "type": "image",
-        "src": "static/game/data/img/map/area01_level_tiles.png"
+        "src": "/static/game/data/img/map/area01_level_tiles.png"
     },
     {
         "name": "level1_tiles",
         "type": "image",
-        "src": "static/game/data/img/map/level1_tiles.png"
+        "src": "/static/game/data/img/map/level1_tiles.png"
     },
     {
         "name": "gripe_run_right",
         "type": "image",
-        "src": "static/game/data/img/sprite/gripe_run_right.png"
+        "src": "/static/game/data/img/sprite/gripe_run_right.png"
     },
     {
         "name": "spinning_coin_gold",
@@ -57,11 +57,11 @@ game.resources = [
     {
         "name": "wheelie_right",
         "type": "image",
-        "src": "static/game/data/img/sprite/wheelie_right.png"
+        "src": "/static/game/data/img/sprite/wheelie_right.png"
     },
     {
         "name": "Game_Map",
         "type": "tmx",
-        "src": "static/game/data/map/Game_Map.tmx"
+        "src": "/static/game/data/map/Game_Map.tmx"
     }
 ];
