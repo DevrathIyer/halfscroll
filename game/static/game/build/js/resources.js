8e55cb66615585d6435243b331eeff475a37dc51
Devrath Iyer <idevspms@gmail.com>
2017-11-12 01:40:08 +0000

    Commit

--- game/static/game/build/js/resources.js
+++ game/static/game/build/js/resources.js
@@ -22,42 +22,42 @@ game.resources = [
     {
         "name": "area01_bkg0",
         "type": "image",
-        "src": "data/img/area01_bkg0.png"
+        "src": "static/game/data/img/area01_bkg0.png"
     },
     {
         "name": "area01_bkg1",
         "type": "image",
-        "src": "data/img/area01_bkg1.png"
+        "src": "static/game/data/img/area01_bkg1.png"
     },
     {
         "name": "title_screen",
         "type": "image",
-        "src": "data/img/gui/title_screen.png"
+        "src": "static/game/data/img/gui/title_screen.png"
     },
     {
         "name": "area01_level_tiles",
         "type": "image",
-        "src": "data/img/map/area01_level_tiles.png"
+        "src": "static/game/data/img/map/area01_level_tiles.png"
     },
     {
         "name": "level1_tiles",
         "type": "image",
-        "src": "data/img/map/level1_tiles.png"
+        "src": "static/game/data/img/map/level1_tiles.png"
     },
     {
         "name": "gripe_run_right",
         "type": "image",
-        "src": "data/img/sprite/gripe_run_right.png"
+        "src": "static/game/data/img/sprite/gripe_run_right.png"
     },
     {
         "name": "spinning_coin_gold",
         "type": "image",
-        "src": "data/img/sprite/spinning_coin_gold.png"
+        "src": "static/game/data/img/sprite/spinning_coin_gold.png"
     },
     {
         "name": "wheelie_right",
         "type": "image",
-        "src": "data/img/sprite/wheelie_right.png"
+        "src": "static/game/data/img/sprite/wheelie_right.png"
     },
     {
         "name": "Game_Map",
