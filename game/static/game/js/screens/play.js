a2dd304e7adfc4b1a00e63640204c82fc0e97229
Devrath Iyer <idevspms@gmail.com>
2017-11-12 00:54:01 +0000

    Commit

--- game/static/game/js/screens/play.js
+++ game/static/game/js/screens/play.js
@@ -4,7 +4,7 @@ game.PlayScreen = me.ScreenObject.extend({
      */
     onResetEvent: function() {
         //load a level
-        me.levelDirector.loadLevel("Game Map");
+        me.levelDirector.loadLevel("Game_Map");
         
         // reset the score
         game.data.score = 0;
