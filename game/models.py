fa5210a42a5b01cd74cbe68b5cacfd9c154a5047
Devrath Iyer <idevspms@gmail.com>
2017-11-12 00:02:42 +0000

    Commit

--- game/models.py
+++ game/models.py
@@ -4,9 +4,20 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     checkpoint = models.FileField(upload_to = 'checkpoints')
     data = models.FileField(upload_to = 'checkpoints/data')
     index = models.FileField(upload_to = 'checkpoints/index')
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
